dc52f3f1ec8c7faf2bcdbb1a5926030357bd4074
cbrooks-dev <connorbrooks04@yahoo.com>
2025-08-08 11:34:16 -0400

    Add handling of answer choice. Need to still show wrong answer and add scoring system

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var current_correct_answer = null
+
 // Mock fallback data
 const mockData = {
   results: [
@@ -33,17 +35,44 @@ const choice2 = document.getElementById("choice-2");
 const choice3 = document.getElementById("choice-3");
 const choice4 = document.getElementById("choice-4");
 
-getTriviaData().then(data => {
-  const result = data.results[0];
-  question.innerHTML = result.question;
+function displayTrivia() {
+  getTriviaData().then(data => {
+    const result = data.results[0];
+    question.innerHTML = result.question;
 
-  // Shuffle answers
-  const answers = [result.correct_answer, ...result.incorrect_answers]
-    .sort(() => Math.random() - 0.5);
+    // Shuffle answers
+    const answers = [result.correct_answer, ...result.incorrect_answers]
+      .sort(() => Math.random() - 0.5);
 
-  // Set text
-  choice1.textContent = answers[0];
-  choice2.textContent = answers[1];
-  choice3.textContent = answers[2];
-  choice4.textContent = answers[3];
-});
+    current_correct_answer = result.correct_answer;
+
+    // Set text
+    choice1.textContent = answers[0];
+    choice2.textContent = answers[1];
+    choice3.textContent = answers[2];
+    choice4.textContent = answers[3];
+  });
+}
+
+displayTrivia()
+
+function checkAnswer(choice) {
+  // Ensure there is a correct answer
+  if (current_correct_answer) {
+    var correct = current_correct_answer;
+    current_correct_answer = null;
+  } else return;
+
+  user_answer = document.getElementById(choice).textContent
+
+  if (user_answer == correct) {
+    document.getElementById(choice).style.color = "green";
+  } else {
+    document.getElementById(choice).style.color = "red";
+  }
+
+  setTimeout(() => {
+    document.getElementById(choice).style.color = "white";
+    displayTrivia(); // Ask a new trivia question
+  }, 2000);
+}
